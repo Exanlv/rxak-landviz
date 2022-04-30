0d21fed868ecb6535bc0e45aa7b5a0d6d8949412
Exan <exan@landviz.nl>
2022-04-30 17:52:59 +0200

    fix: Move credentials around

--- conf/database.php
+++ conf/database.php
@@ -1,11 +1,13 @@
 <?php
 
+use Rxak\Framework\App;
+
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'landviz',
-    'username' => 'homestead',
-    'password' => 'secret',
+    'database' => App::env('DATABASE_NAME'),
+    'username' => App::env('DATABASE_USERNAME'),
+    'password' => App::env('DATABASE_PASSWORD'),
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
